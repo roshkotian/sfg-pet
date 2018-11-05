e2d5092edb2f785a5d5c5c2f0da77bec8444b2e3
Roshan Kotian <Roshan.Kotian@medpro.com>
2018-11-05 14:24:48 -0500

    Initial Project setup

---
+++ src/main/java/roshan/springlearning/sfgpet/SfgPetApplication.java
@@ -0,0 +1,12 @@
+package roshan.springlearning.sfgpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetApplication.class, args);
+    }
+}
---
+++ src/test/java/roshan/springlearning/sfgpet/SfgPetApplicationTests.java
@@ -0,0 +1,16 @@
+package roshan.springlearning.sfgpet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgPetApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
